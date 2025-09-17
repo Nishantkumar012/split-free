77085c59ba4e1c1220534c995a8f69e80400cd9a
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2025-09-17 22:34:57 +0530

    add_expense and ui modify

--- frontend/src/pages/GroupDetail.tsx
+++ frontend/src/pages/GroupDetail.tsx
@@ -136,6 +136,16 @@ function GroupDetail() {
 
       fetchGroupDetail();
    }, [groupId])
+
+
+     // to get rid of refreshing page everytime new expense added
+     // this topic known as state lifting lift of child state to parent
+
+     // call back fn to add expense locally
+    //  const handleAddExpense = (newExpense: Expense) =>{
+             
+    //     setExpense((prev) => [newExpense, ...prev]);
+    //  }
     
   //  {console.log("example ",ex)}
   //  {console.log("members ki detail",members)}
@@ -312,7 +322,17 @@ function GroupDetail() {
                  <h3 className="text-lg font-medium text-gray-900 mb-2">No expenses yet</h3>
                  <p className="text-gray-500 mb-6">Start by adding your first group expense</p>
                  <button
-                   onClick={() => navigate(`/group/${groupId}/add-expense`)}
+                   onClick={() => navigate(`/group/${groupId}/add-expense`, {
+                        
+
+                     // sending this fn handleAddExpense to child
+                     state: {
+                       onExpenseAdded: (newExpense: any) => {
+                              setExpense((prev) => [...prev, newExpense]); // instant update
+                        }                       
+                      }
+                   })
+                  }
                    className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-lg transition-colors duration-200 inline-flex items-center space-x-2"
                  >
                    <Plus className="w-4 h-4" />

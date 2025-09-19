68ef6a31f8bf220900c702a3fe861676e582c746
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2025-09-19 20:58:08 +0530

    one more change

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,7 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const PORT = process.env.PORT || 5000;
-app_1.default.listen(PORT, () => {
-    console.log(` Server is running at port no ${PORT}`);
-});
+const serverless_http_1 = __importDefault(require("serverless-http"));
+exports.default = (0, serverless_http_1.default)(app_1.default);
+// import app from "./app"
+// const PORT = process.env.PORT || 5000;
+// // for it to be worked on vercel
+// // app.listen(PORT, ()=>{
+// //      console.log(` Server is running at port no ${PORT}`);
+// // })

bd94cd372f24c5bcb0682a0a94c75daac9f7afee
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2025-09-19 19:58:20 +0530

    for vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,8 @@ import app from "./app"
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, ()=>{
+// for it to be worked on vercel
+// app.listen(PORT, ()=>{
 
-     console.log(` Server is running at port no ${PORT}`);
-})
+//      console.log(` Server is running at port no ${PORT}`);
+// })

8147e7255db93b59c2506cac9ab042299c50fd3f
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2025-09-19 20:11:11 +0530

    tri

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,5 +1,20 @@
+// prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (!(global as any).prisma) {
+  (global as any).prisma = new PrismaClient();
+}
+
+prisma = (global as any).prisma;
 
 export default prisma;
+
+
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
